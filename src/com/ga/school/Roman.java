3e16398ef6714445485bc84f3dd3e09b00ff0ab9
devhassan19 <devhassan19@email.com>
2026-09-08 13:10:43 +0300

    Submit

--- src/com/ga/school/Roman.java
+++ src/com/ga/school/Roman.java
@@ -1,35 +1,38 @@
 package com.ga.school;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Roman {
 
     public static String intToRoman(int value) {
-        int total= 0;
+        int total = 0;
         String word = "";
         // Here, we're returning an empty string, but you need to build your
         // algorithm and return a String data type accordingly.
 
         Map<String, Integer> symbolValue = new LinkedHashMap<String, Integer>();
-        symbolValue.put("I",1);
-        symbolValue.put("V",5);
-        symbolValue.put("X",10);
-        symbolValue.put("L",50);
-        symbolValue.put("C",100);
-        symbolValue.put("D",500);
-        symbolValue.put("M",1000);
+        symbolValue.put("M", 1000);
+        symbolValue.put("D", 500);
+        symbolValue.put("C", 100);
+        symbolValue.put("L", 50);
+        symbolValue.put("X", 10);
+        symbolValue.put("V", 5);
+        symbolValue.put("I", 1);
 
-       while (total < value) {
-           for (Map.Entry<String, Integer> entry : symbolValue.entrySet()) {
-               if(total + entry.getValue() <= value){
-                   total += entry.getValue();
-                   word = word + entry.getKey();
-               }
+        while (total < value) {
+            for (Map.Entry<String, Integer> entry : symbolValue.entrySet()) {
+                if (entry.getValue() + 2 <= value) {
+                    String subSymbol = entry.getKey() + entry.getKey();
+                    int subVal = entry.getValue();
+                    total += entry.getValue();
+                    word = word + entry.getKey();
+                } else if (total + entry.getValue() <= value) {
+                    total += entry.getValue();
+                    word = word + entry.getKey();
+                }
 
-           }
-       }
+            }
+        }
         return word;
     }
 
@@ -39,5 +42,23 @@ public class Roman {
         System.out.println(intToRoman(9));
         System.out.println(intToRoman(58));
         System.out.println(intToRoman(1994));
+
+//        Input: num = 3
+//        Output: III
+//
+//        Input: num = 4
+//        Output: IV
+//
+//        Input: num = 9
+//        Output: IX
+//
+//        Input: num = 58
+//        Output: LVIII
+//        Explanation: L = 50, V = 5, III = 3.
+//
+//
+//        Input: num = 1994
+//        Output: MCMXCIV
+//        Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
     }
 }
